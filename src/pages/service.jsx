290b35da93c5c72998458b8bd07cf575096a058c
klimalik <klimamalik14@gmail.com>
2024-03-08 22:51:26 +0700

    url view to web demo

--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -7,32 +7,40 @@ export default function Services() {
 
 
   return (
-    <div className="h-max my-6 w-full flex items-center justify-center flex-col gap-20">
+    <div className="h-max my-6 w-full flex items-center justify-center flex-col gap-16">
       <PinContainer
         title="Order Me"
         href="https://wa.me/6283870915417"
+
       >
         <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[16rem] h-[20rem] ">
           <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-white tracking-wide">
-            Web Undangan Pernikahan
+            Wedding Web
           </h3>
           <div className="text-base !m-0 !p-0 font-normal">
             <span className="text-slate-400 text-[.9rem]">
               Menciptakan kenangan yang tak terlupakan. Dengan desain yang elegan✨.
             </span>
           </div>
-          <div className="mt-5">
-            <img src="/undangan.jfif" alt="" className="w-full h-[140px] object-cover rounded-lg" />
+          <div className="mt-3">
+            <img src="/wedding.jpg" alt="" className="w-full h-[140px] object-cover rounded-lg" />
           </div>
         </div>
       </PinContainer>
+
+      <div className="w-[85%] -mt-2">
+        <button className="bg-violet-500 text-[.9rem] px-4 rounded-md text-white py-[4px] hover:bg-violet-700 duration-300">
+          <a href="https://web-wedding-v1.vercel.app/" target="_blank">View</a>
+        </button>
+      </div>
+
       <PinContainer
         title="Order Me"
         href="https://wa.me/6283870915417"
       >
         <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[16rem] h-[20rem] ">
           <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100 tracking-wider">
-            Web Untuk Skripsi
+            Web Skripsi
           </h3>
           <div className="text-base !m-0 !p-0 font-normal">
             <span className="text-slate-500 text-[.9rem]">
@@ -40,10 +48,17 @@ export default function Services() {
             </span>
           </div>
           <div className="mt-6">
-            <img src="/classcrafter.png" alt="" className="w-full h-[140px] object-cover rounded-lg" />
+            <img src="/class.jpg" alt="" className="w-full h-[140px] object-cover rounded-lg" />
           </div>
         </div>
       </PinContainer>
+
+      <div className="w-[85%] -mt-2">
+        <button className="bg-violet-500 text-[.9rem] px-4 rounded-md text-white py-[4px] hover:bg-violet-700 duration-300">
+          <a href="https://classcrafter.vercel.app/" target="_blank">View</a>
+        </button>
+      </div>
+
       <div className="w-[90%] flex flex-col items-center gap-3 mt-4">
         <a href="https://wa.me/6283183679470" className='w-full' target='_blank'>
           <button className="relative w-full inline-flex h-12 overflow-hidden rounded-xl p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
